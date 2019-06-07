4a449d0d649a3c5d902cad403e52b14bb2e03d22
Sharp-on-60hz <51447249+Sharp-on-60hz@users.noreply.github.com>
2019-06-08 00:01:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,19 @@ client.on('message', message => {
     if (message.content === '$Ping') {
     	message.reply('@everyone @dani xd#6502 ');
   	}
+   ``@client.command(pass_context=True)
+async def stats(ctx):
+    author = ctx.message.author
+    servers = list(client.servers)
+    embed = discord.Embed(description=" ", color=0xFFFF)
+    embed.add_field(name="Servers:", value=f"{str(len(servers))}")
+    embed.add_field(name="Users:", value=f"{str(len(set(client.get_all_members())))}")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/528218351378497548/541395759363391513/epic_logo.png")
+    embed.set_footer(text=" | {}".format(bot.user.name), icon_url="https://cdn.discordapp.com/attachments/528218351378497548/541395759363391513/epic_logo.png")
+    await client.say(embed=embed)
+    embed = discord.Embed(title=f"User: {ctx.message.author.name} have used stats command", description=f"User ID: {ctx.message.author.id}", color=0xff9393)
+    embed.set_image(url="https://cdn.discordapp.com/attachments/524655977832775710/541446963887996939/Fade_image.png")
+   await client.send_message(channel, embed=embed)``
    
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
